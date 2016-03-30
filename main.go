e8692263cbbd30b255aababf097646c9d29b04b3
xrayfortytwo <xrayfortytwo@gmail.com>
2016-03-30 22:13:02 +0200

    deleted junk

--- main.go
+++ main.go
@@ -9,26 +9,40 @@ import (
 	"github.com/templategeneration/adaption"
 	"github.com/templategeneration/genjson"
 	"github.com/templategeneration/utils"
+	"log"
+	fpath "path/filepath"
 )
 
 const (
-	LOGPATH              = "./logs/log"
-	DEFAULT_FILE         = "./input/fsml.g4"
-	DESTINATION_TEMPLATE = "./tmp/temp.template"
-	DESTINATION_JSON     = "./tmp/jsondata.json"
+	DEFAULT_FILE         = "/input/fsml.g4"
+	DESTINATION_TEMPLATE = "/tmp/temp.template"
+	DESTINATION_JSON     = "/tmp/jsondata.json"
 )
 
 func main() {
-	var fileName = flag.String("filename", DEFAULT_FILE, "specfiy file location")
-	var destitemp = flag.String("destination_temp", DESTINATION_TEMPLATE, "specify temp. output")
-	var destijson = flag.String("destination_json", DESTINATION_JSON, "specify json output")
+	var cDir = detDir()
+	var fileName = flag.String("filename", cDir+DEFAULT_FILE, "specfiy file location")
+	var destitemp = flag.String("destination_temp", cDir+DESTINATION_TEMPLATE, "specify temp. output")
+	var destijson = flag.String("destination_json", cDir+DESTINATION_JSON, "specify json output")
 	util.CleanUp(*destitemp, *destijson)
 	flag.Parse()
-	util.NewLog(LOGPATH)
-	util.Log.Println("INFO: Read: ", *fileName)
+	log.Println("INFO: Read: ", *fileName)
 	input := util.IOReadFile(*fileName)
 	var s = genjson.GenJson(destijson)
 	var t = &adaption.GenTemp{Gj: s}
 	adaption.RunAdaption(&input, destitemp, t)
 	s.Decode()
+}
+
+func detDir() string {
+	dir, err := fpath.Abs("")
+	check(err)
+	log.Println(dir)
+	return dir
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
